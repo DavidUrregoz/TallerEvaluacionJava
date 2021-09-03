18cd4d3d6ca487bd6d8554d2e104922a07ac84a4
DavidUrregoz <73442281+DavidUrregoz@users.noreply.github.com>
2021-09-03 13:43:48 -0500

    Se agrego y el punto 2 con sus pruebas

--- TallerEvaluacionJava/src/main/java/domain/Puntos.java
+++ TallerEvaluacionJava/src/main/java/domain/Puntos.java
@@ -1,6 +1,11 @@
 package domain;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Puntos {
+    private Scanner sc;
+
     private void punto1(){
         double n1,n2;
         n1=10;
@@ -14,8 +19,28 @@ public class Puntos {
 
     }
 
+    private void punto2(){
+        String n1,n2;
+        sc=new Scanner(System.in);
+        System.out.println("Ingrese Un numero ");
+        n1=sc.next();
+        System.out.println("Ingrese el otro numero ");
+        n2=sc.next();
+        if(n1.matches("[+-]?\\d*(\\.\\d+)?") && n2.matches("[+-]?\\d*(\\.\\d+)?")){
+            double n1double=Double.valueOf(n1);
+            double n2double=Double.valueOf(n2);
+            if(n1double > n2double)
+                System.out.println("El numero mayor es "+ n1double);
+            else if(n1double<n2double)
+                System.out.println("El numero mayor es "+ n2double);
+            else
+                System.out.println("Los numero son iguales ");
+        }else{
+            System.out.println("por favor ingrese solo un numero");
+        }
+    }
+
     public static void main(String[] args) {
         Puntos punto =new Puntos();
-        punto.punto1();
     }
 }
