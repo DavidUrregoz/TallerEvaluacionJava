cbaf4aea8fd7c35e14e9deae0ac4d624c1c68f8b
DavidUrregoz <73442281+DavidUrregoz@users.noreply.github.com>
2021-09-09 20:34:16 -0500

    Se agrega la clase videojuego del punto 18

---
+++ TallerEvaluacionJava/src/main/java/poin18/VideoJuego.java
@@ -0,0 +1,68 @@
+package poin18;
+
+public class VideoJuego {
+    private String titulo;
+    private int horasEstimadas =10;
+    private boolean entregado =false;
+    private String genero;
+    private String compania;
+
+    public VideoJuego() {
+    }
+
+    public VideoJuego(String titulo, int horasEstimadas) {
+        this.titulo = titulo;
+        this.horasEstimadas = horasEstimadas;
+    }
+
+    public VideoJuego(String titulo, int horasEstimadas, String genero, String compania) {
+        this.titulo = titulo;
+        this.horasEstimadas = horasEstimadas;
+        this.genero = genero;
+        this.compania = compania;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getHorasEstimadas() {
+        return horasEstimadas;
+    }
+
+    public void setHorasEstimadas(int horasEstimadas) {
+        this.horasEstimadas = horasEstimadas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCompania() {
+        return compania;
+    }
+
+    public void setCompania(String compania) {
+        this.compania = compania;
+    }
+
+    @Override
+    public String toString() {
+        return "VideoJuego{" +
+                "titulo='" + titulo + '\'' +
+                ", horasEstimadas=" + horasEstimadas +
+                ", entregado=" + entregado +
+                ", genero='" + genero + '\'' +
+                ", compania='" + compania + '\'' +
+                '}';
+    }
+
+}
